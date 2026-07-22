d7ab8fc94e45e5796371e0190b0aa6723f4dd316
Sunil Kumar D R <sunilrajdr11038@gmail.com>
2026-07-22 21:34:30 +0530

    Implemented createEmployee endpoint

--- src/main/java/com/sunitech/learningdeployment01/controller/EmployeeController.java
+++ src/main/java/com/sunitech/learningdeployment01/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sunitech.learningdeployment01.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class EmployeeController {
     @GetMapping("/employee")
     public String fetchEmployee(){
         return "Fetching all the employee details..";
+    }
+
+    @PostMapping("/create")
+    public String createEmployee(){
+        return "Employee created successfully";
     }
 
 }
