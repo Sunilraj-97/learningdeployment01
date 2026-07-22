8f80e0b8cf9fa64eb3799189f477c30fd1b7488c
Sunil Kumar D R <sunilrajdr11038@gmail.com>
2026-07-22 21:18:03 +0530

    Initial spring boot application

---
+++ src/main/java/com/sunitech/learningdeployment01/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.sunitech.learningdeployment01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @GetMapping("/employee")
+    public String fetchEmployee(){
+        return "Fetching all the employee details..";
+    }
+
+}
